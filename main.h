73100a337816be909ff05ae3a564f367113ee2fa
Obarechrispus <obarechrispus@gmail.com>
2024-01-30 06:25:35 -0800

    task0

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int put_char(char c);
-//int printf_char(va_list, val);
+/**int printf_char(va_list, val); */
 int _putchar(char c);
 int _printf(const char *format, ...);
 
